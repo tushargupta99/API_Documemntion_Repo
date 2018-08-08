ff11877cd5eacee13ef2fd5cf229434afe7c79dd
Tushar Gupta <tusharguptacs14.sug@shobhituniversity.ac.in>
2018-08-08 16:53:41 +0530

    Initial Commit
    I have tried to keep it effective but as simple as possible, so that frontend developer can understand this with minimum effort.

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Edwisor Backend App",
+  "version": "0.0.1",
+  "description": "Documentation for the Edwisor Backend app",
+  "title": "EdwisorBackendAPI",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-08-08T11:15:58.811Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
